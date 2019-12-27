3d2a37e966f77eb654030aed561752b6ecc9f8e9
toyourheart163 <blive200@gmail.com>
2019-12-27 10:02:09 +0800

    set ALLOWHOST=['*'] for heroku local test

--- rest/settings/prod.py
+++ rest/settings/prod.py
@@ -41,4 +41,5 @@ if not DEBUG:
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', SECRET_KEY)
 
 # Set to your Domain here (eg. 'django-vue-template-demo.herokuapp.com')
-ALLOWED_HOSTS = ['vue-django-rest-framework.herokuapp.com']
+# ALLOWED_HOSTS = ['vue-django-rest-framework.herokuapp.com']
+ALLOWED_HOSTS = ['*']
